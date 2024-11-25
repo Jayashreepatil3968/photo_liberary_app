3fa57cf49250d52df2c2d66117816c8e76dcaf41
Jayashree4874 <jayashree@ifocussystec.in>
2024-11-25 13:18:56 +0530

    Updates

--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const PHOTOS_PER_PAGE = 8;
-export const STORAGE_KEY = 'photoLibrary';
-export const LOCATIONS = ['New York', 'London', 'Paris', 'Tokyo', 'Sydney'];
-export const PEOPLE = ['John', 'Emma', 'Michael', 'Sarah', 'David'];
